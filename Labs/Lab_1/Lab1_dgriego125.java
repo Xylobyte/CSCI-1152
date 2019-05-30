4605a7ea549a2a65b385108302b81f2d51b6a343
Xylobyte <dxgriego@gmail.com>
2019-05-29 19:21:52 -0600

    Update

---
+++ Labs/Lab_1/Lab1_dgriego125.java
@@ -0,0 +1,19 @@
+/*
+Lab 1
+Donovan Griego
+*/
+
+public class Lab1_dgriego125 {
+    public static void main(String[] args) {
+        float operand1 = 20.0f, operand2 = 99.0f, addResult, subResult, divResult, multResult;
+        addResult = operand1 + operand2;
+        subResult = operand2 - operand1;
+        multResult = operand1 * operand2;
+        divResult = operand2 / operand1;
+        System.out.printf("Donovan Griego\n");
+        System.out.printf("The addition result is : %.1f\n", addResult);
+        System.out.printf("The subtraction result is : %.1f\n", subResult);
+        System.out.printf("The multiply result is : %.1f\n", multResult);
+        System.out.printf("The division result is : %.2f", divResult);
+    }
+}
